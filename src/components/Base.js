c667ee9b0c33b2c9daebf8c61549e3235df1580f
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2021-11-06 09:54:08 +0800

    Added Swipe left to delete

--- src/components/Base.js
+++ src/components/Base.js
@@ -3,130 +3,148 @@ import { useRef } from "react";
 import { InputForm } from "./Input";
 import { List } from "./List";
 
-export default function  Base() {
-    // const title = useRef(null)
-    const btnAll = useRef(null)
-    const btnActive = useRef(null)
-    const btnCompleted = useRef(null)
-    // let tStartX = 0
-    // let tStartY = 0
-    // let tEndX = 0
-    // let tEndY = 0
-    const [todos, setTodos] = useState([
-        // fot testing
-      {text: "List 1",
-      done:false,
-        ref: React.createRef(0)},
-      {text: "List 2",
-      done:false,
-      ref: React.createRef(1)},
-      {text: "List 3",
-      done:false,
-      ref: React.createRef(2)},
-    ])
+export default function Base() {
+  const btnAll = useRef(null)
+  const btnActive = useRef(null)
+  const btnCompleted = useRef(null)
+ 
+  let tStartX = 0
+  let tStartY = 0
+  let tEndX = 0
+  let tEndY = 0
+  const [todos, setTodos] = useState([
+    // fot testing
+    {
+      text: "List 1",
+      done: false,
+      ref: React.createRef(0)
+    },
+    {
+      text: "List 2",
+      done: false,
+      ref: React.createRef(1)
+    },
+    {
+      text: "List 3",
+      done: false,
+      ref: React.createRef(2)
+    },
+  ])
 
-    
-  
-    const add = (text, done, ref) => {
-      done = false
-      ref = React.createRef(todos.length + 1)
-      const newList = [...todos, {text, done, ref}]
+  const add = (text, done, ref) => {
+    done = false
+    ref = React.createRef(todos.length + 1)
+    const newList = [...todos, { text, done, ref }]
+    setTodos(newList)
+    console.log(newList)
+  }
+
+  const completed = index => {
+
+    const newList = [...todos]
+    console.log(newList[index].done)
+    if (!newList[index].done) {
+      newList[index].done = true
       setTodos(newList)
-      console.log(newList)
-    }
-  
-    const completed = index => {
-    
-      const newList = [...todos]
-      console.log(newList[index].done)
-      if (!newList[index].done){
-          newList[index].done = true
-          setTodos(newList)
-      } else {
-        newList[index].done = false
-        setTodos(newList)
-      }
-    }   
-
-    const del = index => {
-      const newList = [...todos]
-      newList.splice(index, 1)
+    } else {
+      newList[index].done = false
       setTodos(newList)
     }
+  }
 
-    const showAll = () => {
-        for (var i in todos) {
-          if((todos[i].ref.current.className).includes(" remove-list")) {
-            todos[i].ref.current.className = (todos[i].ref.current.className).replace(/ remove-list/g, "")
-          }
-        }
-    }
-    
-    const showActive = () => {
+  const del = index => {
+    console.log(todos[index])
+    todos[index].ref.current.style.display = "none"
+    todos[index].text = ""
+    console.log(todos[index])
+  }
 
-        for (var i in todos) {
-            if(todos[i].done === true) {
-              todos[i].ref.current.className += " remove-list"
-            }
-            if(todos[i].done === false ) {
-              if((todos[i].ref.current.className).includes(" remove-list")) {
-                todos[i].ref.current.className = (todos[i].ref.current.className).replace(/ remove-list/g, "")
-              }
-            }
-        }
-    }
-
-    const showCmpleted  = () => {
-        for (var i in todos) {
-          if(todos[i].done === false) {
-            todos[i].ref.current.className += " remove-list"
-          } 
-          if(todos[i].done === true ) {
-            if((todos[i].ref.current.className).includes(" remove-list")) {
-              todos[i].ref.current.className = (todos[i].ref.current.className).replace(/ remove-list/g, "")
-            } 
-          }
+  const showAll = () => {
+    for (var i in todos) {
+      if ((todos[i].ref.current.className).includes(" remove-list")) {
+        todos[i].ref.current.className = (todos[i].ref.current.className).replace(/ remove-list/g, "")
       }
     }
-  
-    // function swipeDelete(index) {
-    //   let dX = tEndX - tStartX
-    //   let dY = tEndY - tStartY
-     
-      
-    //   if (Math.abs(dX) > Math.abs(dY)) {
-    //     if (dX < 0) {
-    //       let currentCls = todos[index].ref
-          // todos[index].ref.current.className = currentCls + " swipe-delete"
-          // todos[index].ref.current.className = currentCls
-    //       setTimeout(() => {
-    //         del(index)
-          
-    //       }, 1000)
-          
-    //     }
-    //   }
-    // }
+  }
 
-    return (
-        <div>
-         
-       <InputForm add= {add}/>
+  const showActive = () => {
+
+    for (var i in todos) {
+      if (todos[i].done === true) {
+        todos[i].ref.current.className += " remove-list"
+      }
+      if (todos[i].done === false) {
+        if ((todos[i].ref.current.className).includes(" remove-list")) {
+          todos[i].ref.current.className = (todos[i].ref.current.className).replace(/ remove-list/g, "")
+        }
+      }
+    }
+  }
+
+  const showCmpleted = () => {
+    for (var i in todos) {
+      if (todos[i].done === false) {
+        todos[i].ref.current.className += " remove-list"
+      }
+      if (todos[i].done === true) {
+        if ((todos[i].ref.current.className).includes(" remove-list")) {
+          todos[i].ref.current.className = (todos[i].ref.current.className).replace(/ remove-list/g, "")
+        }
+      }
+    }
+  }
+
+  const touchStart = (e) => {
+    tStartX = e.changedTouches[0].screenX 
+    tStartY= e.changedTouches[0].screenY
+  }
+
+  function swipeDelete(index, e) {
+    tEndX = e.changedTouches[0].screenX 
+    tEndY= e.changedTouches[0].screenY
+
+    let dX = tEndX - tStartX
+    let dY = tEndY - tStartY
+
+    console.log(dX, dY)
+    if (Math.abs(dX) > Math.abs(dY)) {
+
+      if (dX < 0) {
+        todos[index].ref.current.className += " swipe-delete"
         
-        {todos.map((todo, index) => (
-            <List todo = {todo}
-                  index = {index}
-                  del = {del}
-                  completed = {completed} />
-        ))}
+          setTimeout(() => {
+            del(index)
+          },510)
+        
        
 
-        <div className="card-stats card-bg">
-                <a href="#" ref={btnAll} className="card-stats-active" onClick={()=> {showAll()}}> All </a>
-                <a href="#" ref={btnActive} className="card-stats-deactive" onClick={()=> {showActive()}}> Active </a>
-                <a href="#" ref={btnCompleted} className="card-stats-deactive" onClick={()=> {showCmpleted()}}> Completed </a>
-        </div>
+       
 
-      </div>  
-    )
+      }
+    }
   }
+
+  return (
+    <div>
+
+      <InputForm add={add} />
+
+      {todos.map((todo, index) => (
+        <List todo={todo}
+          index={index}
+          del={del}
+          completed={completed}
+          touchStart={touchStart}
+          swipeDelete={swipeDelete} />
+      ))}
+
+
+      <div className="card-stats card-bg">
+        <a href="#" ref={btnAll} className="card-stats-active" onClick={() => { showAll() }}> All </a>
+        <a href="#" ref={btnActive} className="card-stats-deactive" onClick={() => { showActive() }}> Active </a>
+        <a href="#" ref={btnCompleted} className="card-stats-deactive" onClick={() => { showCmpleted() }}> Completed </a>
+      </div>
+
+    </div>
+  )
+}
--- src/components/List.js
+++ src/components/List.js
@@ -1,24 +1,14 @@
 import React from "react";
 
-export function List ({index, todo, completed, del}) {
+export function List ({index, todo, completed, del, touchStart, swipeDelete}) {
 
     return (
-        <div key={index} id={index} className="card-list card-bg add-list" ref={todo.ref} >
+        <div key={index} id={index} className="card-list card-bg add-list" ref={todo.ref} onTouchStart={(e) => {touchStart(e)}} onTouchEnd={(e) => {swipeDelete(index,e)}}>
         <button className="card-done-icon" >
             <svg style={{display: "inline", zIndex: "-999"}} className="svg-icon" onClick={()=> {completed(index)}} xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="18" height="18"><path  d="M12,0A12,12,0,1,0,24,12,12,12,0,0,0,12,0Zm0,22A10,10,0,1,1,22,12,10,10,0,0,1,12,22Z"/></svg>
             {/* <svg style={{display: "block", zIndex: "200"}} xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="18" height="18"><path d="M22.319,4.431,8.5,18.249a1,1,0,0,1-1.417,0L1.739,12.9a1,1,0,0,0-1.417,0h0a1,1,0,0,0,0,1.417l5.346,5.345a3.008,3.008,0,0,0,4.25,0L23.736,5.847a1,1,0,0,0,0-1.416h0A1,1,0,0,0,22.319,4.431Z"/></svg> */}
             </button>
-            <div className="card-list-text card-text" 
-
-            // onTouchStart={(e)=> {
-            //       tStartX = e.changedTouches[0].screenX 
-            //       tStartY= e.changedTouches[0].screenY}} 
-            //       onTouchEnd = {(e)=> {
-            //         tEndX = e.changedTouches[0].screenX 
-            //         tEndY= e.changedTouches[0].screenY
-            //         swipeDelete(index)
-            //       }}
-                     style={{textDecoration: todo.done ? "line-through" : ""}}>
+            <div className="card-list-text card-text" style={{textDecoration: todo.done ? "line-through" : ""}}>
             {todo.text}
             </div>
             <button href="#" className="card-delete-icon" >
